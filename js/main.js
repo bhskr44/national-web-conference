bf9c15113f5aaba930076fb96e95fd2922a46348
BHASKAR RANJAN BORA <30873007+bhskr44@users.noreply.github.com>
2022-10-20 18:27:39 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -83,3 +83,15 @@ for (let i = 0; i < featuredSpeakers.length; i += 1) {
     </p>
     </div>`;
 }
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById('slidedown').style.top = '0';
+  } else {
+    document.getElementById('slidedown').style.top = '-50px';
+  }
+}
